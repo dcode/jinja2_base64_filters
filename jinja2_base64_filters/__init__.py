365783390b0ecb42e2399b5a666a2d22f65d9516
Timothée GERMAIN <timothee@lumapps.com>
2019-04-23 11:55:38 +0200

    Bump version to 0.1.2

--- jinja2_base64_filters/__init__.py
+++ jinja2_base64_filters/__init__.py
@@ -2,6 +2,6 @@
 
 from .jinja2_base64_filters import Base64Filters
 
-__version__ = "0.1.0"
+__version__ = "0.1.2"
 __author__ = "Timothée GERMAIN <timothee@lumapps.com>"
 __all__ = ["Base64Filters"]
